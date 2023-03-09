99b9606645bce17c728c63f3bbaf5147e4938fe8
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-03-10 03:38:06 +0530

    Added ToDoList Model with MongoDB

--- server/models/models.go
+++ server/models/models.go
@@ -1,1 +1,10 @@
 package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type ToDoList struct {
+	ID     primitive.ObjectID `json:"id,omitempty" bson:"id,omitempty"`
+	Task   string             `json:"task,omitempty" bson:"task,omitempty"`
+	Status bool               `json:"status,omitempty" bson:"status,omitempty"`
+}
+
